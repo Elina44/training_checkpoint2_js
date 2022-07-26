6037db4e2c955805e471b0caa631adac13beef49
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-07-26 10:30:21 +0200

    init

---
+++ training_checkpoint2_js/src/component/Header.js
@@ -0,0 +1,13 @@
+function Header () {
+    const name = "Welcome to React to the games";
+
+    return (
+        <div>
+            <h1>
+                {name}
+            </h1>
+        </div>
+    )
+}
+
+export default Header;
